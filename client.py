238024782010701ab9fb2d748a5ea41a54b4bd2c
FancyFeast666 <connorkuziemko2021@gmail.com>
2025-03-12 12:04:18 -0400

    updating gui to have connection verification

--- client.py
+++ client.py
@@ -5,6 +5,8 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 import customtkinter
+import matplotlib.pyplot as plt
+
 
 API_URL = "http://192.168.1.107:5000/stats"
 def fetch_stats():
@@ -15,10 +17,35 @@ def fetch_stats():
     except requests.exceptions.RequestException:
         return None
 
-#print(fetch_stats())
+def connect():
+    global IP, PORT
+    IP = entryIP.get()
+    PORT = entryPort.get()
+    IP = "http://" + IP + ":" + PORT + "/status"
+    response = requests.get(IP, timeout=2)
+    if response.status_code == 200:
+        print( response.json())
 
 if __name__ == "__main__":
     window = tk.Tk()
     window.title("Server Monitoring")
-    window.geometry("700x700")
+    window.geometry("500x500")
 
+
+    labelIP = tk.Label(window, text="Enter IP Address of Server")
+    labelIP.grid(row=1, column=0, ipady=10)
+
+    entryIP = tk.Entry(window)
+    entryIP.grid(row =2, column=0, ipady=10)
+
+    labelPort = tk.Label(window, text="Enter Port Address of Server")
+    labelPort.grid(row=3, column=0, ipady=10)
+
+    entryPort = tk.Entry(window)
+    entryPort.grid(row=4, column=0, ipady=10)
+
+    connect = tk.Button(window, text="Connect", command=connect)
+    connect.grid(row=9, column=0, ipady=10)
+
+
+    window.mainloop()
